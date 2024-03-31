65239c7e09c65b030d232f6b855363b7fbb0c4f6
转正的兔子 <icemaka2021@outlook.com>
2024-03-31 12:38:04 +0800

    feat: init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>LeetClone</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.png' />
+        <meta
+					name='description'
+					content='Web application that contains leetcode problems and video solutions'
+				/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type AuthProps = {};
+
+const Auth: React.FC<AuthProps> = () => {
+    return <div>Auth Page</div>;
+}
+export default Auth;
